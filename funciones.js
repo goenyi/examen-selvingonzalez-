35a1ab819ee31aac3b2187c4208d635e8d253731
unknown <goenyig1999@gmail.com>
2017-03-10 12:07:28 -0600

    pongame buenos puntos

---
+++ funciones.js
@@ -0,0 +1,7 @@
+var bacoB = angular.module("bacob",[]);
+bacob.controller("mensaje",function($scope){});
+
+Bacob.controller("operacion",function($scope){
+	$scope.numero1 = 25;
+	$scope.numer2 = 62;
+	$scope.multiplicacion = scope.numero1*scope.numer2
